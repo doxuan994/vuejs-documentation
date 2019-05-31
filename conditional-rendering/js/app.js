9c850873583c24cdc5daa160899898e07fac97ff
Xuan Do <doxuancntp@gmail.com>
2019-05-31 16:31:14 -0400

    Commit.

---
+++ conditional-rendering/js/app.js
@@ -0,0 +1,14 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        awesome: true,
+        ok: true,
+        loginType: 'username'
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
